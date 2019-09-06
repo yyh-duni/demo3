55cb3c980a899b7b5b86f9aeb0892934dcb8a94f
whc <https://github.com/WLSJAVA/SZ07.git>
2019-09-06 15:56:08 +0800

    11111

--- src/main/java/com/aaa/demo3/controller/My.java
+++ src/main/java/com/aaa/demo3/controller/My.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class My {
     public static void main(String[] args) {
         System.out.println("sdfghj");
+        System.out.println(111);
         Scanner sc=new Scanner(System.in);
     }
     public void test(){}
